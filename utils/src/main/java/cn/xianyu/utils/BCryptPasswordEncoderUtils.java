2e366c3bd2832c02b3a35e544e8197de15494246
咸鱼至尊 <54784104+SaltedFish-Extreme@users.noreply.github.com>
2020-02-18 12:06:27 +0800

    第四次提交
    上传工具类代码

---
+++ utils/src/main/java/cn/xianyu/utils/BCryptPasswordEncoderUtils.java
@@ -0,0 +1,17 @@
+package cn.xianyu.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class BCryptPasswordEncoderUtils {
+    private static BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+    public static String encodePassword(String password) {
+        return bCryptPasswordEncoder.encode(password);
+    }
+
+    public static void main(String[] args) {
+        String password = "1234567890";
+        String pwd = encodePassword(password);
+        System.out.println(pwd);
+    }
+}
